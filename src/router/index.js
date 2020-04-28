57b20c291922ae90b5f17398db7751dddfa6c10d
gazorpazorp85 <paolo.groppi@gmail.com>
2020-04-28 19:04:51 +0300

    minor fix

--- src/router/index.js
+++ src/router/index.js
@@ -34,8 +34,8 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.NODE_ENV === 'production' ? process.env.PUBLIC_URL : process.env.BASE_URL,
+  // mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
 
