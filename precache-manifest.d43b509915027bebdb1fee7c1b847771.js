05622eaf4b40c6604f51bfe8096c0e117246ef45
gazorpazorp85 <paolo.groppi@gmail.com>
2020-05-04 12:54:50 +0300

    'Deploy'

---
+++ precache-manifest.d43b509915027bebdb1fee7c1b847771.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ec311939ec6b692280",
+    "url": "/witw/css/app.57f55fb7.css"
+  },
+  {
+    "revision": "d8a016a38197ece09f56",
+    "url": "/witw/css/chunk-vendors.902ebb66.css"
+  },
+  {
+    "revision": "4c8f447011eef80831b45edb1e5971e0",
+    "url": "/witw/fonts/nunito.4c8f4470.ttf"
+  },
+  {
+    "revision": "08e53a516d2ba719d98da46c49b3c369",
+    "url": "/witw/fonts/nunitobold.08e53a51.ttf"
+  },
+  {
+    "revision": "63e59cfd7d055797810d8938ab863cf5",
+    "url": "/witw/img/arrow.63e59cfd.png"
+  },
+  {
+    "revision": "e0a3afd2fcba66281b51cf8e839a4ccc",
+    "url": "/witw/img/white-arrow.e0a3afd2.png"
+  },
+  {
+    "revision": "9d80b63204d5b7824203bd3b215b61f6",
+    "url": "/witw/index.html"
+  },
+  {
+    "revision": "36ec311939ec6b692280",
+    "url": "/witw/js/app.380da7b5.js"
+  },
+  {
+    "revision": "d8a016a38197ece09f56",
+    "url": "/witw/js/chunk-vendors.8d862aa4.js"
+  },
+  {
+    "revision": "f7a2a0b35e36b0d0b6267da2351eb66e",
+    "url": "/witw/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/witw/robots.txt"
+  }
+]);
